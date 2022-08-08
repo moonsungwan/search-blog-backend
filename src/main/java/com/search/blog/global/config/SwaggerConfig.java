694d6a07961149ffb18b1e4beebaac3e8a62a7cb
moonsungwan <grape0122@naver.com>
2022-08-08 12:51:38 +0900

    스웨거 수정

--- src/main/java/com/search/blog/global/config/SwaggerConfig.java
+++ src/main/java/com/search/blog/global/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -31,7 +32,7 @@ public class SwaggerConfig {
                  .securityContexts(Arrays.asList(securityContext()))
                  .securitySchemes(Arrays.asList(apiKey()))
                  .select()
-                 .apis(RequestHandlerSelectors.any())
+                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                  .paths(PathSelectors.any())
                  .build()
                  .apiInfo(metaData())
